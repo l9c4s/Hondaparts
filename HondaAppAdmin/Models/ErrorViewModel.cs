b563d0541f2f5f22bba34e974d49606ba8a194cb
Lucas <lucas.santos@koode.io>
2022-11-16 21:55:37 -0300

    Colocando projeto em MVC

---
+++ HondaAppAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HondaAppAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
